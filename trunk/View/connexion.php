11d1444cf2bf32f6b3bdd6b7e0cf10193f4aff51
chabert.antoine@gmail.com <chabert.antoine@gmail.com@6c46abcc-b069-bf07-ab9d-6dcb911f53fb>
2013-11-26 21:02:50 +0000

    Ajout de deux nouvelles vue : La vue principal de connexion et la vue d'erreur 404 - Ajout et modification du controller Index - Ajout de quelques images et styles pour la page de connexion

---
+++ trunk/View/connexion.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" media="screen" type="text/css" title="design" href="CSS/design.css" />
+    <link rel="shortcut icon" type="image/png" href="Images/mafavicon.png" />
+    <title></title>
+</head>
+<body>
+    <header class="connexion">
+        <h1>Connexion aux emplois du temps - I.U.T Montpellier 2</h1>
+    </header>
+    <div id="content" class="connexion">
+        <div id="fomulaire">
+            <p> This is the connection page ... Fuck ce site !</p>
+        </div>
+    </div>
+    <footer class="connexion">
+    </footer>
+    </div>
+</body>
+</html>
