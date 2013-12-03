af4a0ef85da3c6d5d0a14599dfcb44c304cbae88
chabert.antoine@gmail.com <chabert.antoine@gmail.com@6c46abcc-b069-bf07-ab9d-6dcb911f53fb>
2013-12-03 13:39:58 +0000

    Modification de la vue moncompte

--- trunk/View/moncompte.php
+++ trunk/View/moncompte.php
@@ -1,5 +1,5 @@
 <?php include_once 'Templates/header.php'; ?>
     <div id="content">
-        <p>Bienvenue sur votre compte</p>
+        <p>Bienvenue sur votre compte <?php echo $var["user"]->getPrenom(); ?> <?php echo $var["user"]->getNom(); ?></p>
     </div>
 <?php include_once 'Templates/footer.php'; ?>
