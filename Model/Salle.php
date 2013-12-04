a13d5c4fe42563bcda5924f008770a9e443b2683
thomas.leguelinel@gmail.com <thomas.leguelinel@gmail.com@6c46abcc-b069-bf07-ab9d-6dcb911f53fb>
2013-12-04 16:01:48 +0000

    model de salle imcoplet, TODO SAVE,LOAD,SET

--- Model/Salle.php
+++ Model/Salle.php
@@ -1,1 +1,60 @@
 <?php
+class Model_Salle implements Model
+{
+	// Attribut:
+	private $numeroSalle;
+	private $nomBat;
+	private $capacite;
+	private $specifications;
+	
+	
+	/**
+	 * Constructeur d'un utilisateur
+	 * @param
+	 */
+	public function __construct ($numeroSalle=null,$nomBat=null,$capacite=null,$specification=null) {
+		$this->numeroSalle=$numeroSalle;
+		$this->nomBat=$nomBat;
+		$this->capacite=$capacite;
+		$this->specifications=$specifications;
+		
+	}
+	 public function getNumeroSalle(){
+	 	return $this->numeroSalle;
+	 }
+	 public function getNomBatiment(){
+	 	return $this->nomBat;
+	 }
+	 public function getCapacite(){
+	 	return $this->capacite;
+	 }
+	 public function getSpecification(){
+	 	return $this->specifications;
+	 }
+	 
+	
+	public function save() {
+		$res = App_Mysql::getInstance()->query("SELECT numeroSalle,nomBat FROM Salle WHERE numeroSalle='".App_Mysql::getInstance()->quote($this->numeroSalle)."' AND nomBat='".App_Mysql::getInstance()->quote($this->nomBat)."'");
+		if($tuple = App_Mysql::getInstance()->fetchArray($res)) {
+			$res = App_Mysql::getInstance()->query("UPDATE Salle SET capacite='".mysql_real_escape_string($this->capacite)."', specifications='".mysql_real_escape_string($this->specifications)."' WHERE numeroSalle='".$this->numeroSalle."'AND nomBat='".$this->nomBat."';");
+		}
+		else{
+			if($this->numeroSalle!=null && $this->nomBat!=null && $this->capacite!=null && $this->specifications!=null){
+				$res = App_Mysql::getInstance()->query("INSERT INTO Salle (numeroSalle,nomBat,capacite,specifications) VALUES('".App_Mysql::getInstance()->quote($this->numeroSalle)."','".App_Mysql::getInstance()->quote($this->)."','".App_Mysql::getInstance()->quote($this->prenom)."','".App_Mysql::getInstance()->quote($this->mdp)."','".App_Mysql::getInstance()->quote($this->email)."','".App_Mysql::getInstance()->quote($this->type).")");
+			}
+		}
+	}
+	
+	// return null si aucun user n'a cet id sinon une instance de la classe User
+	public static function load($id) {
+		$ret=null;
+		$res = App_Mysql::getInstance()->query("SELECT * FROM Personne WHERE identifiant='".App_Mysql::getInstance()->quote($id)."'");
+		if($tuple = App_Mysql::getInstance()->fetchArray($res)) {
+			$ret=new Model_User($tuple["identifiant"],$tuple["email"],$tuple["nom"],$tuple["prenom"],$tuple["mdp"],$tuple["droit"]);
+		}
+		return $ret;
+	}
+	
+	
+}
+?>
