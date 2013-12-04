379f8105da7c51e5a152f57b19c3a44296132ba3
zaragoza.arnaud@gmail.com <zaragoza.arnaud@gmail.com@6c46abcc-b069-bf07-ab9d-6dcb911f53fb>
2013-12-04 15:30:10 +0000

    ADD View/consultation_recherche.php + MAJ diverse

--- trunk/View/affichageEdt.php
+++ trunk/View/affichageEdt.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" media="screen" type="text/css" title="design" href="CSS/design.css" />
-	<link rel="shortcut icon" type="image/png" href="Images/mafavicon.png" />
-	<title></title>
-</head>
-<body>
-	<header class="connexion">
-		<h1>Connexion aux emplois du temps - I.U.T Montpellier 2</h1>
-	</header>
-	<div id="content" class="affichageEdt">
-	<table border="1">
+<?php include_once 'Templates/header.php'; ?>
+    <div id="content">
+        <table border="1">
 		<caption> Emploi du temps </caption> 
 		<tr> 
 			<th> Horraire </th>
@@ -24,19 +13,15 @@
 			<th> Dimanche </th>
 		</tr>
 <?php
-	for($i=0;$i<48;i++){
+	for($i=0;$i<48;$i++){
 		echo "<tr>";
 		echo "<th> 8:00 </th>";
-		for($j=0;$j<7;i++){
+		for($j=0;$j<7;$i++){
 			echo "<td></td>";
 		}
 		echo "</tr>";
 	}
 ?>
-	</TABLE> 
-	</div>
-	<footer class="connexion">
-	</footer>
-	</div>
-</body>
-</html>
+	</table> 
+    </div>
+<?php include_once 'Templates/footer.php'; ?>
