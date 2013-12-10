ee0df6ddd51e913839ca1557fb51425efbaac534
levan.laurent@gmail.com <levan.laurent@gmail.com@6c46abcc-b069-bf07-ab9d-6dcb911f53fb>
2013-12-10 23:11:35 +0000

    modification du footer +css

--- Templates/footer.php
+++ Templates/footer.php
@@ -1,5 +1,7 @@
     <footer>
-        <p>Footer : Site réalisé par des gens ...</p>
+        <p id=footer-txt>Site réalisé par <a class=link href="http://antoine-chabert.fr/">CHABERT Antoine</a>, LEVAN Laurent, Zaragoza Arnaud, Leguelinel Thomas</p>
+        
+        <p id=footer-copyright>Copyright © 2013 ALTA Tous droits réservés.</p>
     </footer>
     </div>
 </body>
