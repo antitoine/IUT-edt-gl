c79b0f596b14d6b8d0935022bc7964e4b74f4bed
levan.laurent@gmail.com <levan.laurent@gmail.com@6c46abcc-b069-bf07-ab9d-6dcb911f53fb>
2013-12-10 14:47:08 +0000

    mis a jour de la vue mesinfo.php, rajout du type et de l'email

--- View/mesinfos.php
+++ View/mesinfos.php
@@ -1,10 +1,11 @@
 <?php include_once 'Templates/header.php'; ?>
 	<div class="content">
-	<h1>Mes informations</h1>
+	<h3>Mes informations</h3>
 	<p>
 		ID : <?php echo $var["user"]->getId() ?><br />
 		Nom : <?php echo $var["user"]->getNom() ?><br />
 		Prenom : <?php echo $var["user"]->getPrenom()?><br />
+		e-mail : <?php echo $var["user"]->getEmail()?><br />
 		Type Compte : 
 		<?php 
 			if($var["user"]->getType()==0)echo "Etudiant";
@@ -12,6 +13,8 @@
 			else echo "Admin";
 		?>
 	</p>
+	
+	
 	</div>
 
 <?php include_once 'Templates/footer.php'; ?>
