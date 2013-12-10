99021338d83b41fc68141e39e78e815e57e29add
thomas.leguelinel@gmail.com <thomas.leguelinel@gmail.com@6c46abcc-b069-bf07-ab9d-6dcb911f53fb>
2013-12-10 14:52:14 +0000

    Controller foireux de MesInfos vesion 2

--- Controller/MesInfos.php
+++ Controller/MesInfos.php
@@ -11,36 +11,34 @@ class Controller_Modifications implements Controller
 		}
 	}
 	
-	public function modificationMotDePasse()
+	public function modifier()
 	{
 		if (!empty(App_Request::getParam("envoi"))) {
-			if (!empty(trim(App_Request::getParam("mdp_Modif")))) { // si le champs n'est pas vide
+			$user_id= trim(App_Request::getParam("user_id"));// recupere l'id de l'utilisateur courant
+			$user= Model_User::load($user_id);//cree une instance d'un utilisatur
+			if($user->getType()==0){
+				$user=Model_Etudiant::load($user_id);
+			}
+			if (!empty(trim(App_Request::getParam("mdp_Modif")))) {
 				$mdp = trim(App_Request::getParam("mdp_Modif"));// recupere le nouveau mdp
-				$user_id= trim(App_Request::getParam("user_id"));// recupere l'id de l'utilisateur courant
-				$user= ModelUser::load($user_id);//cree une instance d'un utilisatur
-				$user->setMdp($mdp);// change le mot de passe dans user
-				$user->save();// modifie dans la base de donnée ^^
+				$user->setMdp($mdp);
+				$var["mdp"]=$mdp;
 			}
+			
+			if(!empty(trim(App_Request::getParam("email_Modif")))){
+				$email = trim(App_Request::getParam("email_Modif"));// recupere le nouveau email
+				$syntaxe="#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$#";
+				if(preg_match($syntaxe,$email)){
+					$user->setEmail($email);
+					$var["email"]=$email;
+				}
+			}
+			$user->save();// modifie dans la base de donnée ^^
+			$var["user"]=$user;
+			$view = new App_View('mesinfos.php');
+			$view->render($var);
+			
 		}	
-		$view = new App_View('mesinfos.php');
-		$view->render(null);
-	}
-	
-	
-	public function modificationEmail()
-	{
-		$email = trim(App_Request::getParam("email_Modif"));// recupere le nouveau email
-		$syntaxe="#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$#";
-		if (!empty(App_Request::getParam("envoi")) && strpreg_match($Syntaxe,$email) ) {
-			if (!empty(trim(App_Request::getParam("email_Modif")))) { // si le champs n'est pas vide
-								$user_id= trim(App_Request::getParam("user_id"));// recupere l'id de l'utilisateur courant
-				$user= ModelUser::load($user_id);//cree une instance d'un utilisatur
-				$user->setEmail($mdp);// change le mot de passe dans user
-				$user->save();// modifie dans la base de donnée ^^
-			}
-		}
-		$view = new App_View('mesinfos.php');
-		$view->render(null);
-	}
+			
 }
-?>
+			
