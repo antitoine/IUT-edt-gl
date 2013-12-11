e8579fb8ce9e4aed7357175ea541cb2810afcd9d
chabert.antoine@gmail.com <chabert.antoine@gmail.com@6c46abcc-b069-bf07-ab9d-6dcb911f53fb>
2013-12-11 14:23:36 +0000

    Rectification de bug

--- trunk/Templates/footer.php
+++ trunk/Templates/footer.php
@@ -1,4 +1,4 @@
-    <footer>
+    <footer class="footer">
         <p id=footer-txt>Site réalisé par <a class=link href="http://antoine-chabert.fr/">CHABERT Antoine</a>, LEVAN Laurent, Zaragoza Arnaud, Leguelinel Thomas</p>
         
         <p id=footer-copyright>Copyright © 2013 ALTA Tous droits réservés.</p>
