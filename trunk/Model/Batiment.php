660a2fc39211b22663b8781c5519b61f38727e07
levan.laurent@gmail.com <levan.laurent@gmail.com@6c46abcc-b069-bf07-ab9d-6dcb911f53fb>
2013-12-11 15:05:37 +0000

    ajout de commentaires dans le modele Batiment

--- trunk/Model/Batiment.php
+++ trunk/Model/Batiment.php
@@ -1,19 +1,23 @@
 <?php
 class Model_Batiment implements Model
 {
-	// Attribut:
+	// Attribut :
 	private $nom;
 	private $adresse;
 	
 	/**
-	* Constructeur d'un batiments 
-	* @param 
-	*/
+	 * Constructeur d'un batiment
+	 * @param String $nom 
+	 * @param String $adresse
+	 */
 	public function __construct ($nom=null,$adresse=null) {
-		$this->nom=$nom;
+		$this->nom=$nom; 
 		$this->adresse=$adresse;
 	}
 	
+	/**
+	 * creer un Batiment ou met à jour les données dans la base de données
+	 */
 	public function save() {
 		$res = App_Mysql::getInstance()->query("SELECT nom FROM Batiment WHERE nom='".App_Mysql::getInstance()->quote($this->nom)."';");
 		if(App_Mysql::getInstance()->fetchArray($res)) {
@@ -26,7 +30,12 @@ class Model_Batiment implements Model
 		}
 	}
 	
-	// return null si aucun batiment n'a ce nom sinon une instance de la classe Batiment
+	/**
+	 *  fonction qui recupere les données d'un batiment avec le nom du Batiment a partir de la base de données
+	 *  return null si aucun batiment n'a ce nom sinon une instance de la classe Batiment
+	 *  @param String $nom du batiment
+	 *  @return $ret une instance Batiment 
+	 */
 	public static function load($nom) {
 		$ret=null;
 		$res = App_Mysql::getInstance()->query("SELECT * FROM Batiment WHERE nom='".App_Mysql::getInstance()->quote($nom)."';");
@@ -36,11 +45,16 @@ class Model_Batiment implements Model
 		return $ret;
 	}
 	
-	// les getter:
+	/**
+	 * GETTER 
+	 */
 	public function getNom(){ return $this->nom; }
 	public function getAdresse(){ return $this->adresse; }
 	
-	// les setter:
+	/**
+	 * SETTER
+	 * @param String $s
+	 */
 	public function setType($s){ $this->nom=$s; }
 	public function setIdGrp($s){ $this->adresse=$s; }
 	
