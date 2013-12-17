15ac9338203736476c4c00b0894869c9b5f636f4
zaragoza.arnaud@gmail.com <zaragoza.arnaud@gmail.com@6c46abcc-b069-bf07-ab9d-6dcb911f53fb>
2013-12-17 13:20:11 +0000

    Modification Model/Edt

--- Model/EmploiDuTemps.php
+++ Model/EmploiDuTemps.php
@@ -64,9 +64,6 @@ class Model_EmploiDuTemps
             $array_cours[0][$i] = new Model_Cours($tuple['idCours'],$tuple['idProf'],$tuple['nomMatiere'],$tuple['Groupe'],$tuple['numeroSalle'],$tuple['nomBat'],$tuple['heureDebut'],$tuple['heureFin'],$tuple['Date'],$tuple['descriptionCours']);
             $i++;
         }
-        if (count($array_cours[0])==0) {
-            return null;
-        }
         return $array_cours;
     }
 
